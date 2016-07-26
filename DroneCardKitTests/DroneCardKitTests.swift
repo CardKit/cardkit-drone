e54ce708f9e9f12c225314e203a08152ef643094
Justin Weisz <jweisz@us.ibm.com>
2016-07-26 09:07:09 -0400

    recreated project to work better with carthage

--- DroneCardKitTests/DroneCardKitTests.swift
+++ DroneCardKitTests/DroneCardKitTests.swift
@@ -2,7 +2,7 @@
 //  DroneCardKitTests.swift
 //  DroneCardKitTests
 //
-//  Created by ismails on 6/17/16.
+//  Created by Justin Weisz on 7/26/16.
 //  Copyright © 2016 IBM. All rights reserved.
 //
 
