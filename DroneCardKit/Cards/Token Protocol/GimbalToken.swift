4cedba2d476ba0861390b702b4ac0eee042e566c
JUSTIN G. MANWEILER <jmanweiler@us.ibm.com>
2016-12-09 15:43:13 -0500

    starting to normalize the GimbalToken spec to new input types

--- DroneCardKit/Cards/Token Protocol/GimbalToken.swift
+++ DroneCardKit/Cards/Token Protocol/GimbalToken.swift
@@ -15,6 +15,37 @@ import PromiseKit
 
 public protocol GimbalToken {
     func reset() -> Promise<Void>
-    func rotateToAbsoluteAngle(pitch: Float, roll: Float, yaw: Float) -> Promise<Void>
-    func rotateToRelativeAngle(pitch: Float, roll: Float, yaw: Float) -> Promise<Void>
+    func rotate(yaw: DCKAngle?, pitch: DCKAngle?, roll: DCKAngle?, relative: Bool) -> Promise<Void>
+}
+
+public extension GimbalToken {
+    
+    func rotate(yaw: DCKAngle) -> Promise<Void> {
+        return rotate(yaw: yaw, pitch: nil, roll: nil, relative: false)
+    }
+    
+    func rotate(yaw: DCKAngle, pitch: DCKAngle) -> Promise<Void> {
+        return rotate(yaw: yaw, pitch: pitch, roll: nil, relative: false)
+    }
+    
+    func rotate(yaw: DCKAngle, pitch: DCKAngle, roll: DCKAngle) -> Promise<Void> {
+        return rotate(yaw: yaw, pitch: pitch, roll: roll, relative: false)
+    }
+    
+    func rotate(pitch: DCKAngle) -> Promise<Void> {
+        return rotate(yaw: nil, pitch: pitch, roll: nil, relative: false)
+    }
+    
+    func rotate(pitch: DCKAngle, roll: DCKAngle) -> Promise<Void> {
+        return rotate(yaw: nil, pitch: pitch, roll: roll, relative: false)
+    }
+    
+    func rotate(orientation: DCKOrientation) -> Promise<Void> {
+        return rotate(yaw: orientation.yaw, pitch: orientation.pitch, roll: orientation.roll, relative: false)
+    }
+    
+    func rotate(orientation: DCKOrientation, relative: Bool) -> Promise<Void> {
+        return rotate(yaw: orientation.yaw, pitch: orientation.pitch, roll: orientation.roll, relative: relative)
+    }
+    
 }
