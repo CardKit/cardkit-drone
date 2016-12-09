66e825f36adfca72a5a99f2c5c5839374e9f77f9
JUSTIN G. MANWEILER <jmanweiler@us.ibm.com>
2016-12-09 14:28:19 -0500

    typo

--- DroneCardKitTests/DroneCardKitTests.swift
+++ DroneCardKitTests/DroneCardKitTests.swift
@@ -17,7 +17,7 @@ class DroneCardKitTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is ca lled after the invocation of each test method in the class.
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
