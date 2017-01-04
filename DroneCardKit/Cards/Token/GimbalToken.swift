d260463f3bf12200131c9606c74c1695143db99d
JUSTIN G. MANWEILER <jmanweiler@us.ibm.com>
2017-01-04 11:08:54 -0500

    gimbal and camera tokens

--- DroneCardKit/Cards/Token/GimbalToken.swift
+++ DroneCardKit/Cards/Token/GimbalToken.swift
@@ -11,47 +11,45 @@ import Foundation
 import CardKit
 import CardKitRuntime
 
-import PromiseKit
-
 public protocol GimbalToken {
     func reset() -> Promise<Void>
-    func rotate(yaw: DCKAngle?, pitch: DCKAngle?, roll: DCKAngle?, relative: Bool) -> Promise<Void>
+    func rotate(yaw: DCKAngle?, pitch: DCKAngle?, roll: DCKAngle?, relative: Bool, completionHandler: DroneTokenCompletionHandler?)
 }
 
 public extension GimbalToken {
-    func rotate(yaw: DCKAngle) -> Promise<Void> {
-        return rotate(yaw: yaw, pitch: nil, roll: nil, relative: false)
+    func rotate(yaw: DCKAngle, completionHandler: DroneTokenCompletionHandler?){
+        return rotate(yaw: yaw, pitch: nil, roll: nil, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(pitch: DCKAngle) -> Promise<Void> {
-        return rotate(yaw: nil, pitch: pitch, roll: nil, relative: false)
+    func rotate(pitch: DCKAngle, completionHandler: DroneTokenCompletionHandler?){
+        return rotate(yaw: nil, pitch: pitch, roll: nil, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(roll: DCKAngle) -> Promise<Void> {
-        return rotate(yaw: nil, pitch: nil, roll: roll, relative: false)
+    func rotate(roll: DCKAngle, completionHandler: DroneTokenCompletionHandler?) {
+        return rotate(yaw: nil, pitch: nil, roll: roll, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(yaw: DCKAngle, pitch: DCKAngle) -> Promise<Void> {
-        return rotate(yaw: yaw, pitch: pitch, roll: nil, relative: false)
+    func rotate(yaw: DCKAngle, pitch: DCKAngle, completionHandler: DroneTokenCompletionHandler?) {
+        return rotate(yaw: yaw, pitch: pitch, roll: nil, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(yaw: DCKAngle, roll: DCKAngle) -> Promise<Void> {
-        return rotate(yaw: yaw, pitch: nil, roll: roll, relative: false)
+    func rotate(yaw: DCKAngle, roll: DCKAngle, completionHandler: DroneTokenCompletionHandler?) {
+        return rotate(yaw: yaw, pitch: nil, roll: roll, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(pitch: DCKAngle, roll: DCKAngle) -> Promise<Void> {
-        return rotate(yaw: nil, pitch: pitch, roll: roll, relative: false)
+    func rotate(pitch: DCKAngle, roll: DCKAngle, completionHandler: DroneTokenCompletionHandler?) {
+        return rotate(yaw: nil, pitch: pitch, roll: roll, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(yaw: DCKAngle, pitch: DCKAngle, roll: DCKAngle) -> Promise<Void> {
-        return rotate(yaw: yaw, pitch: pitch, roll: roll, relative: false)
+    func rotate(yaw: DCKAngle, pitch: DCKAngle, roll: DCKAngle, completionHandler: DroneTokenCompletionHandler?) {
+        return rotate(yaw: yaw, pitch: pitch, roll: roll, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(attitude: DCKAttitude) -> Promise<Void> {
-        return rotate(yaw: attitude.yaw, pitch: attitude.pitch, roll: attitude.roll, relative: false)
+    func rotate(attitude: DCKAttitude, completionHandler: DroneTokenCompletionHandler?) {
+        return rotate(yaw: attitude.yaw, pitch: attitude.pitch, roll: attitude.roll, relative: false, completionHandler: completionHandler)
     }
     
-    func rotate(attitude: DCKAttitude, relative: Bool) -> Promise<Void> {
-        return rotate(yaw: attitude.yaw, pitch: attitude.pitch, roll: attitude.roll, relative: relative)
+    func rotate(attitude: DCKAttitude, relative: Bool, completionHandler: DroneTokenCompletionHandler?) {
+        return rotate(yaw: attitude.yaw, pitch: attitude.pitch, roll: attitude.roll, relative: relative, completionHandler: completionHandler)
     }
 }
