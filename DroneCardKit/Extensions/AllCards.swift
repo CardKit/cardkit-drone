41d03745c3e4dd9e230754cf29dd34c1c4122d14
Kristina M Brimijoin <kbrimij@us.ibm.com>
2017-02-28 11:03:56 -0500

    started all cards function

---
+++ DroneCardKit/Extensions/AllCards.swift
@@ -0,0 +1,21 @@
+//
+//  AllCards.swift
+//  DroneCardKit
+//
+//  Created by Kristina M Brimijoin on 2/28/17.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+import Foundation
+import CardKit
+
+extension DroneCardKit {
+    
+    public func allCards() -> [ActionCardDescriptor] {
+        return [
+            DroneCardKit.Action.Movement.Simple.FlyForward,
+            DroneCardKit.Action.Movement.Simple.Hover,
+            DroneCardKit.Action.Movement.Simple.Land
+        ]
+    }    
+}
