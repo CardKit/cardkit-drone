1a15e1b042c023d14851149987006d5b0cd009a2
Kristina M Brimijoin <kbrimij@us.ibm.com>
2017-02-28 15:50:13 -0500

    deleting AllCards file and putting functions into DroneCardKit file; change function names from allCards to allDescriptors;

--- DroneCardKit/DroneCardKit/DroneCardKit.swift
+++ DroneCardKit/DroneCardKit/DroneCardKit.swift
@@ -18,6 +18,46 @@ public struct DroneCardKit {
 // MARK: - Action Cards
 
 extension DroneCardKit {
+    
+    public static func allDescriptors() -> [ActionCardDescriptor] {
+        return [
+            DroneCardKit.Action.Movement.Simple.FlyForward,
+            DroneCardKit.Action.Movement.Simple.Hover,
+            DroneCardKit.Action.Movement.Simple.Land,
+            DroneCardKit.Action.Movement.Location.Circle,
+            DroneCardKit.Action.Movement.Location.CircleRepeatedly,
+            DroneCardKit.Action.Movement.Location.FlyTo,
+            DroneCardKit.Action.Movement.Location.ReturnHome,
+            DroneCardKit.Action.Movement.Sequence.FlyPath,
+            DroneCardKit.Action.Movement.Sequence.Pace,
+            DroneCardKit.Action.Tech.Camera.RecordVideo,
+            DroneCardKit.Action.Tech.Camera.TakeHDRPhoto,
+            DroneCardKit.Action.Tech.Camera.TakePhoto,
+            DroneCardKit.Action.Tech.Camera.TakePhotoBurst,
+            DroneCardKit.Action.Tech.Camera.TakePhotos,
+            DroneCardKit.Action.Tech.Gimbal.PanBetweenLocations,
+            DroneCardKit.Action.Tech.Gimbal.PointAtFront,
+            DroneCardKit.Action.Tech.Gimbal.PointAtGround,
+            DroneCardKit.Action.Tech.Gimbal.PointAtLocation,
+            DroneCardKit.Action.Tech.Gimbal.PointAtMovement,
+            DroneCardKit.Action.Tech.Gimbal.PointInDirection
+        ]
+    }
+    
+    public static func allDescriptorsGrouped() -> [String:[ActionCardDescriptor]] {
+        let cards = allDescriptors()
+        var grouped: [String: [ActionCardDescriptor]] = [:]
+        
+        for card in cards {
+            if grouped[card.path.description] == nil {
+                grouped[card.path.description] = [card]
+            } else {
+                grouped[card.path.description]?.append(card)
+            }
+        }
+        return grouped
+    }
+    
     /// Contains descriptors for Action cards
     public struct Action {
         fileprivate init() {}
