0bb2b289723a2b5e47fa04b203cf66628dc00551
Justin Weisz <jweisz@us.ibm.com>
2017-03-02 17:26:16 -0500

    removed all Watson stuff and migrated it to watson-card-kit

--- DroneCardKit/DroneCardKit/DroneCardKit.swift
+++ DroneCardKit/DroneCardKit/DroneCardKit.swift
@@ -605,102 +605,6 @@ extension DroneCardKit.Action.Tech {
     }
 }
 
-extension DroneCardKit.Action {
-    /// Contains descriptors for Action/Think cards
-    public struct Think {
-        fileprivate init() {}
-    }
-}
-
-extension DroneCardKit.Action.Think {
-    public static let DetectObject = ActionCardDescriptor(
-        name: "Detect an Object",
-        subpath: "Think",
-        inputs: [
-            InputSlot(name: "Objects", descriptor: CardKit.Input.Text.TextString, isOptional: false),
-            InputSlot(name: "Confidence", descriptor: CardKit.Input.Numeric.Real, isOptional: true),
-            InputSlot(name: "Frequency", descriptor: CardKit.Input.Time.Periodicity, isOptional: false)
-        ],
-        tokens: [
-            TokenSlot(name: "Camera", descriptor: DroneCardKit.Token.Camera),
-            TokenSlot(name: "Telemetry", descriptor: DroneCardKit.Token.Telemetry),
-            TokenSlot(name: "WatsonVisualRecognition", descriptor: DroneCardKit.Token.Watson.VisualRecognition)
-        ],
-        yields: [
-            Yield(type: DCKDetectedObject.self)
-        ],
-        yieldDescription: "Yields the object that was detected",
-        ends: true,
-        endsDescription: "Ends when one of the given objects was detected",
-        assetCatalog: CardAssetCatalog(description: "Detects an object in the camera's field of view using Watson"))
-    
-    /*
-    public static let DetectInAir = ActionCardDescriptor(
-        name: "Detect In Air",
-        subpath: "Think",
-        inputs: [
-            InputSlot(name: "Image", descriptor: CardKit.Input.Media.Image, isOptional: false)
-        ],
-        tokens: [
-            TokenSlot(name: "Camera", descriptor: DroneCardKit.Token.Camera),
-            TokenSlot(name: "Gimbal", descriptor: DroneCardKit.Token.Gimbal)
-        ],
-        yields: [Yield(type: DCKCoordinate3D.self)],
-        yieldDescription: "Yields the coordinate of the detected object",
-        ends: true,
-        endsDescription: "Ends when the object has been detected",
-        assetCatalog: CardAssetCatalog(description: "Detect an object in the air"))
-    
-    public static let DetectOnGround = ActionCardDescriptor(
-        name: "Detect On Ground",
-        subpath: "Think",
-        inputs: [
-            InputSlot(name: "Image", descriptor: CardKit.Input.Media.Image, isOptional: false)
-        ],
-        tokens: [
-            TokenSlot(name: "Camera", descriptor: DroneCardKit.Token.Camera),
-            TokenSlot(name: "Gimbal", descriptor: DroneCardKit.Token.Gimbal)
-        ],
-        yields: [Yield(type: DCKCoordinate2D.self)],
-        yieldDescription: "Yields the coordinate of the detected object",
-        ends: true,
-        endsDescription: "Ends when the object has been detected",
-        assetCatalog: CardAssetCatalog(description: "Detect an object on the ground"))
-    
-    public static let TrackInAir = ActionCardDescriptor(
-        name: "Track In Air",
-        subpath: "Think",
-        inputs: [
-            InputSlot(name: "Image", descriptor: CardKit.Input.Media.Image, isOptional: false)
-        ],
-        tokens: [
-            TokenSlot(name: "Camera", descriptor: DroneCardKit.Token.Camera),
-            TokenSlot(name: "Gimbal", descriptor: DroneCardKit.Token.Gimbal)
-        ],
-        yields: [Yield(type: DCKCoordinate3D.self)],
-        yieldDescription: "Yields the coordinate of the detected object",
-        ends: false,
-        endsDescription: nil,
-        assetCatalog: CardAssetCatalog(description: "Track an object in the air"))
-    
-    public static let TrackOnGround = ActionCardDescriptor(
-        name: "Track On Ground",
-        subpath: "Think",
-        inputs: [
-            InputSlot(name: "Image", descriptor: CardKit.Input.Media.Image, isOptional: false)
-        ],
-        tokens: [
-            TokenSlot(name: "Camera", descriptor: DroneCardKit.Token.Camera),
-            TokenSlot(name: "Gimbal", descriptor: DroneCardKit.Token.Gimbal)
-        ],
-        yields: [Yield(type: DCKCoordinate2D.self)],
-        yieldDescription: "Yields the coordinate of the detected object",
-        ends: false,
-        endsDescription: nil,
-        assetCatalog: CardAssetCatalog(description: "Track an object on the ground"))
- */
-}
-
 // MARK: - Input Cards
 
 extension DroneCardKit {
@@ -963,21 +867,6 @@ extension DroneCardKit.Token {
         subpath: nil,
         isConsumed: false,
         assetCatalog: CardAssetCatalog(description: "Drone telemetry token"))
-}
-
-extension DroneCardKit.Token {
-    /// Contains descriptors for Watson cards
-    public struct Watson {
-        fileprivate init() {}
-    }
-}
-
-extension DroneCardKit.Token.Watson {
-    public static let VisualRecognition = TokenCardDescriptor(
-        name: "Watson Visual Recognition",
-        subpath: "Watson",
-        isConsumed: false,
-        assetCatalog: CardAssetCatalog(description: "Watson Visual Recognition token"))
 }
 
 
